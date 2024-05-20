41cb8e73858dd0fbf71f5c8eb633d00162eb5301
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-05-20 08:48:27 +0300

    React-Hooks-Props-Basics-Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+const user = {
+  name: "Richard",
+  city: "Nairobi",
+  bio: "Richard Otieno is a highly experienced civil and construction engineer with over 8 years of expertise in highways design, contract administration, and supervision. He is currently pursuing further education in software engineering to combine his technical skills with modern tech solutions. Richard is passionate about integrating IoT in smart cities and utilizing data analytics for efficient infrastructure management.",
+  github: "https://github.com/RICHARD-ODHIAMBO-OTIENO",
+  linkedin: "https://www.linkedin.com/in/richard-otieno-7b210b7b/"
+};
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color="firebrick" />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
